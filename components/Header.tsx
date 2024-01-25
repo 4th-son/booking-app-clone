9fbb78a61b30bc4c714d56706aa300e913b35d89
Michael ogozi <michealogozi@gmail.com>
2024-01-25 15:43:37 +0300

    finished the hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Dialog, Popover, Transition } from "@headlessui/react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   ChatBubbleLeftIcon,
@@ -11,6 +11,7 @@ import {
 import { ChevronDownIcon, HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { Fragment, useState } from "react";
+import { cn } from "@/lib/utils";
 
 const products = [
   {
@@ -177,6 +178,75 @@ function Header() {
               <span className="sr-only">Close Menu</span>
               <XMarkIcon className=" h-6 w-6" aria-hidden="true" />
             </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button
+                        className=" flex w-full items-center justify-between
+                       rounded-lg py-2 pl-3 pr-3.5  text-base font-semibold leading-7  text-white hover:bg-blue-800"
+                      >
+                        Stays
+                        <ChevronDownIcon
+                          className={cn(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 float-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className=" block rounded-lg py-2 pl-6 pr-6 text-sm font-semibold leading-7  text-white hover:bg-blue-800"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className=" -mx-3  block rounded-lg px-3 py-2 text-base font-semibold leading-7  text-white  hover:bg-blue-800  "
+                >
+                  Flights
+                </a>
+                <a
+                  href="#"
+                  className=" -mx-3  block rounded-lg px-3 py-2 text-base font-semibold leading-7  text-white  hover:bg-blue-800  "
+                >
+                  Car Rentals
+                </a>
+                <a
+                  href="#"
+                  className=" -mx-3  block rounded-lg px-3 py-2 text-base font-semibold leading-7  text-white  hover:bg-blue-800  "
+                >
+                  Attractions
+                </a>
+                <a
+                  href="#"
+                  className=" -mx-3  block rounded-lg px-3 py-2 text-base font-semibold leading-7  text-white  hover:bg-blue-800  "
+                >
+                  Flights + Hotels
+                </a>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className=" -mx-3  block rounded-lg px-3 py-2.5 text-base font-semibold leading-7  text-white  hover:bg-blue-800  "
+                  >
+                    Login
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </Dialog.Panel>
       </Dialog>
